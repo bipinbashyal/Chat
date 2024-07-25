f1c0d9a9701b031de1d4e6e3564c4f93cc8b8b4f
Bipin Bashyal <bipinbashyal321@gmail.com>
2024-07-25 16:50:07 +0545

    Backend initialized

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const app = require("./app");
+require("dotenv").config();
+
+const server = http.createServer(app);
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log(`server is running on: http://localhost:${PORT}`);
+});
