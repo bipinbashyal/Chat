e4e2a41923ff2b8d35cdf28dda16cbad7abcb93c
Bipin Bashyal <bipinbashyal321@gmail.com>
2024-08-10 12:12:22 +0545

    add functionalities in chat model

--- Backend/models/chat/chat.model.js
+++ Backend/models/chat/chat.model.js
@@ -1,8 +1,43 @@
 const chatModel = require("./chat.mongo");
 
+const addChat = async (chat) => {
+  try {
+    return await chatModel.create(chat);
+  } catch (err) {
+    throw err;
+  }
+};
+
+const editChat = async (chat, chatId) => {
+  try {
+    return await chatModel.findByIdAndUpdate(chatId, chat);
+  } catch (err) {
+    throw err;
+  }
+};
+
+const removeChat = async (chatId) => {
+  try {
+    return await chatModel.findByIdAndDelete(chatId);
+  } catch (err) {
+    throw err;
+  }
+};
+
 const getAllChat = async (userId) => {
   try {
-    return await chatModel.find({ members: userId });
+    return await chatModel.find({ members: userId }).sort({ updatedAt: -1 });
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+const updateChat = async (chatId) => {
+  try {
+    return await chatModel.findById(chatId, {
+      $currentDate: { updatedAt: true },
+    });
   } catch (err) {
     console.log(err);
     throw err;
@@ -10,5 +45,9 @@ const getAllChat = async (userId) => {
 };
 
 module.exports = {
+  addChat,
   getAllChat,
+  updateChat,
+  editChat,
+  removeChat,
 };
