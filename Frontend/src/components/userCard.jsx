29bce32173b4be2df46426e988b89760e4ec5d58
Bipin Bashyal <bipinbashyal321@gmail.com>
2024-09-15 08:07:54 +0545

    handle multiple clicks error

--- Frontend/src/components/userCard.jsx
+++ Frontend/src/components/userCard.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 
 const UserCard = ({ user, buttonText, handleClick }) => {
+  const [loading, setLoading] = useState(false);
+  const handleMouseClick = async (user) => {
+    setLoading(true);
+    await handleClick(user);
+    setLoading(false);
+  };
   return (
     <div className="flex flex-wrap flex-col items-center justify-center bg-white gap-2 px-8 py-2 min-w-48">
       <div className="w-20 h-20 rounded-full bg-red-400">
@@ -9,13 +15,27 @@ const UserCard = ({ user, buttonText, handleClick }) => {
       </div>
       <div className="font-bold">{user.username}</div>
       <div className="text-slate-400">{user.email}</div>
-      <Button
+      {loading ? (
+        <Button className={"opacity-75 cursor-not-allowed"}>
+          {buttonText}
+        </Button>
+      ) : (
+        <Button
+          onClick={() => {
+            setLoading(true);
+            handleMouseClick(user);
+          }}
+        >
+          {buttonText}
+        </Button>
+      )}
+      {/* <Button
         onClick={() => {
           handleClick(user);
         }}
       >
         {buttonText}
-      </Button>
+      </Button> */}
     </div>
   );
 };
